6d441e7bc83e8c1aef4eae0b5c4140deb5ba8f1c
Asier Lostalé <asier.lostale@openbravo.com>
2018-07-27 13:48:31 +0200

    test case for incorrect assertion for padded batching

---
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/PaddedBatchFetchTestCase.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2014 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hibernate.bugs;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.bugs.model.City;
+import org.hibernate.bugs.model.Country;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
+import org.junit.Test;
+
+public class PaddedBatchFetchTestCase extends BaseCoreFunctionalTestCase {
+
+	// Add your entities here.
+	@Override
+	protected Class[] getAnnotatedClasses() {
+		return new Class[] { Country.class, City.class };
+	}
+
+	@Override
+	protected void configure(Configuration configuration) {
+		super.configure(configuration);
+
+		configuration.setProperty(AvailableSettings.SHOW_SQL, Boolean.TRUE.toString());
+		configuration.setProperty(AvailableSettings.FORMAT_SQL, Boolean.TRUE.toString());
+
+		configuration.getProperties().setProperty(AvailableSettings.BATCH_FETCH_STYLE, "PADDED");
+		configuration.getProperties().setProperty(AvailableSettings.DEFAULT_BATCH_FETCH_SIZE, "15");
+
+		// configuration.setProperty( AvailableSettings.GENERATE_STATISTICS, "true" );
+	}
+
+	// Add your tests, using standard JUnit.
+	@Test
+	public void paddedBatchFetchTest() throws Exception {
+		createData();
+
+		Session s = openSession();
+		Transaction tx = s.beginTransaction();
+
+		List<City> allCities = s.createQuery("from City", City.class).list();
+
+		System.out.println("all cities " + allCities.get(0).getCountry().getName());
+		tx.commit();
+		s.close();
+	}
+
+	private void createData() {
+		Session s = openSession();
+		Transaction tx = s.beginTransaction();
+
+		// Having DEFAULT_BATCH_FETCH_SIZE=15
+		// results in batchSizes = [15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+		// Let's create 11 countries so batch size 15 will be used with padded values,
+		// this causes to have to remove 4 elements from list
+		int numberOfCountries = 11;
+		for (int i = 0; i < numberOfCountries; i++) {
+			Country c = new Country("Country-" + i);
+			s.save(c);
+			s.save(new City("City" + i, c));
+		}
+
+		tx.commit();
+		s.close();
+	}
+}
---
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/model/City.java
@@ -0,0 +1,66 @@
+package org.hibernate.bugs.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "city")
+public class City {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "name")
+	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "country_id")
+	private Country country;
+
+	public City() {
+	}
+
+	public City(String name, Country country) {
+		super();
+		this.name = name;
+		this.country = country;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return name + " (" + (country == null ? "?" : country.getName()) + ")";
+	}
+
+}
